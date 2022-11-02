88bcee31c4d892ba89b689bae1ac3d37aa5707d8
Thomas Bejar <113930376+tabejar@users.noreply.github.com>
2022-11-02 17:55:03 +0000

    dynamic functions

---
+++ dynamic_functions.py
@@ -0,0 +1,56 @@
+
+def check_3Digits(list1):
+  # return number in range(100, 1000)
+  three_digit_list = []
+  
+  for n in list1:
+    if n in range(100,1000):
+      three_digit_list.append(n)
+      # return True
+    else:
+      pass
+
+  return three_digit_list
+  
+  
+  # pass
+  
+########################################################################################################################
+# Dynamic Functions Practice #1
+# Create a function (all_positives) that returns True if all the values in a list are positive, and False if at least one of the values is negative. Create a list named numbers with positive and negative values.
+def all_positives(numbers):
+  for n in numbers:
+    if n >= 0:
+      return True
+    elif numbers <= 0:
+      return False
+
+  
+# Don't call the function, you just need to define it.
+
+
+########################################################################################################################
+# Dynamic Functions Practice #2
+# Create a function (sum_less) that adds the numbers of a list (stored in the variable numbers) as long as they are greater than 0 and less than 1000, and returns the result of said sum.
+def sum_less(numbers):
+  sum = 0
+  for a in numbers:
+    if numbers > 0 and numbers < 1000:
+      sum(numbers)
+      
+    
+
+
+########################################################################################################################
+# Dynamic Functions Practice #3
+# Create a function (count_even) that counts the number of even numbers that exist in a list (numbers), and returns the result of said count.
+
+# def count_even(numbers):
+#   for n in count_even:
+#     if n %2 == 0:
+      
+      
+   
+      
+     
+  
--- main.py
+++ main.py
@@ -1,26 +1,36 @@
-coffee_prices =[('cappucino', 1.5,),
-               ('expresso',1.2),
-               ('mocha',1.9)]
+from dynamic_functions import all_positives, sum_less
+# coffee_prices =[('cappucino', 1.5,),
+#                ('expresso',1.2),
+#                ('mocha',1.9)]
 
-#retrieve the prices and price from the above tuple
+# #retrieve the prices and price from the above tuple
 
-for coffee, price in coffee_prices:
-  print(f"{coffee} costs ${price}")
-# create a function for retrieving the highest priced coffee and name
-print("Welcome Jonesy")
-expensive = max(coffee_prices)
-print(f"The most expensive coffee is {coffee} and it costs ${price}")
+# for coffee, price in coffee_prices:
+#   print(f"{coffee} costs ${price}")
+# # create a function for retrieving the highest priced coffee and name
+# print("Welcome Jonesy Fortnite")
+# expensive = max(coffee_prices)
+# print(f"The most expensive coffee is {coffee} and it costs ${price}")
 
-def most_expensive_coffee(coffee_prices):
-  highest_prices = 0
-  my_most_expensive_coffee = ''
-  for coffee, price in coffee_prices:
-    if price > highest_prices:
-      highest_prices = price
-      my_most_expensive_coffee = coffee
-  else:
-    pass
-  return(my_most_expensive_coffee, highest_prices)
+# def most_expensive_coffee(coffee_prices):
+#   highest_prices = 0
+#   my_most_expensive_coffee = ''
+#   for coffee, price in coffee_prices:
+#     if price > highest_prices:
+#       highest_prices = price
+#       my_most_expensive_coffee = coffee
+#   else:
+#     pass
+#   return(my_most_expensive_coffee, highest_prices)
   
-print(most_expensive_coffee(coffee_prices))
-  
+# print(most_expensive_coffee(coffee_prices))
+
+
+# fortnite = count_even.count([4,6,2])
+# print(fortnite)
+
+result = all_positives([89, -5, 7])
+print(result)
+
+result = sum_less([45,32,23])
+print(result)
