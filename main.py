6dccf9b614173f934b3fdc40bec75663df1042c3
Thomas Bejar <113930376+tabejar@users.noreply.github.com>
2022-11-02 17:04:06 +0000

    coffee functions

--- main.py
+++ main.py
@@ -4,3 +4,23 @@ coffee_prices =[('cappucino', 1.5,),
 
 #retrieve the prices and price from the above tuple
 
+for coffee, price in coffee_prices:
+  print(f"{coffee} costs ${price}")
+# create a function for retrieving the highest priced coffee and name
+print("Welcome Jonesy")
+expensive = max(coffee_prices)
+print(f"The most expensive coffee is {coffee} and it costs ${price}")
+
+def most_expensive_coffee(coffee_prices):
+  highest_prices = 0
+  my_most_expensive_coffee = ''
+  for coffee, price in coffee_prices:
+    if price > highest_prices:
+      highest_prices = price
+      my_most_expensive_coffee = coffee
+  else:
+    pass
+  return(my_most_expensive_coffee, highest_prices)
+  
+print(most_expensive_coffee(coffee_prices))
+  
